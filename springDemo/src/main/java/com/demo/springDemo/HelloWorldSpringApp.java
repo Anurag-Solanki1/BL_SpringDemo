ca0edfbc2395167463922b3faa711c869b419f6f
Anurag-Solanki1 <solankimayank028.com>
2025-03-01 23:31:53 +0530

    MVC Controller

--- springDemo/src/main/java/com/demo/springDemo/HelloWorldSpringApp.java
+++ springDemo/src/main/java/com/demo/springDemo/HelloWorldSpringApp.java
@@ -5,11 +5,24 @@ package com.demo.springDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.demo.springDemoSTS")
 public class HelloWorldSpringApp {
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldSpringApp.class, args);
+    }
+
+    @Controller
+    public static class HelloWorldController {
+
+        @GetMapping("/hello")
+        public String sayHello(Model model) {
+            model.addAttribute("message", "Hello from BridgeLabz!");
+            return "hello";  // Refers to hello.html inside 'templates' folder
+        }
     }
 }
